7c854f56c134742168b453847dc959c728398f7d
Raymond <musicsmithnz@gmail.com>
2018-04-08 23:28:32 +1200

    html5

---
+++ html/html_elements.js
@@ -0,0 +1,79 @@
+const categories=[
+    "flowContent", "sectioningContent", 
+    "sectioningRoot", "palpableContent" 
+];
+const flowElements=[
+    "a", "abbr", "address", "area", 
+    "article", "aside", "audio", "b", 
+    "bdi", "bdo", "blockquote", "br", 
+    "button", "canvas", "cite", "code", 
+    "data", "date", "datalist", "del", 
+    "details", "dfn", "dialog", "div", 
+    "dl", "em", "embed", "fieldset", 
+    "figure", "footer", "form", "h1", 
+    "h2", "h3", "h4", "h5", "h6", 
+    "header", "hr", "i", "iframe", 
+    "img", "input", "ins", "kbd", 
+    "label", "link", "main", "map", 
+    "mark", "math", "menu", "meta", 
+    "meter", "nav", "noscript", 
+    "object", "ol", "output", "p", 
+    "picture", "pre", "progress", "q", 
+    "ruby", "s", "samp", "script", 
+    "section", "select", "small", 
+    "span", "strong", "style", "sub", 
+    "sup", "svg", "table", "template", 
+    "textarea", "time", "u", "ul", 
+    "var", "video", "wbr",
+    "autonomous_custom_elements"
+];
+const headingElements=[
+    "h1", "h2", "h3", "h4", "h5", "h6"    
+];
+const sectioningElements=[
+    "article", "aside", "nav", "section"
+];
+const sectioningRoots=[
+    "blockquote", "body", "details",
+    "dialog", "fieldset", "figure",
+    "td"
+];
+const metaElements=[
+    "base", "link", "meta", "noscript", 
+    "script", "style", "template", "title"
+];
+const interactiveElements=[
+    "a", "audio", "button", "details", 
+    "embed", "iframe", "img", "input", 
+    "label", "object", "select", 
+    "textarea", "video"
+];
+const phrasingElements=[
+    "a", "abbr", "address", "area", "article", 
+    "aside", "audio", "b", "bdi", "bdo", 
+    "blockquote", "br", "button", "canvas", 
+    "cite", "code", "data", "date", "datalist", 
+    "del", "details", "dfn", "dialog", "div", 
+    "dl", "em", "embed", "fieldset", "figure", 
+    "footer", "form", "h1", "h2", "h3", "h4", 
+    "h5", "h6", "header", "hr", "i", "iframe", 
+    "img", "input", "ins", "kbd", "label", 
+    "link", "main", "map", "mark", "math", 
+    "menu", "meta", "meter", "nav", "noscript", 
+    "object", "ol", "output", "p", "picture", 
+    "pre", "progress", "q", "ruby", "s", "samp", 
+    "script", "section", "select", "small", 
+    "span", "strong", "style", "sub", "sup", 
+    "svg", "table", "template", "textarea", 
+    "time", "u", "ul", "var", "video", "wbr"
+];
+const groupingContent=[
+    "p", "address", "hr", "pre", "blockquote",
+    "ol", "ul", "li", "dl", "dt", "dd", "figure",
+    "figcaption", "main", "div"
+];
+const embeddedContentElement=[
+    "audio", "canvas", "embed", "iframe",
+    "img", "math", "object", "picture",
+    "svg", "video"
+];
